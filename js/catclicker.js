5f74d0d481807024be86b6f2fde9f61cb352e81a
abhijeet <abhijeetpnwr@gmail.com>
2016-08-29 12:28:42 +0530

    Double Cat clciker modification

--- js/catclicker.js
+++ js/catclicker.js
@@ -2,8 +2,12 @@
 $(document).ready(function() 
 	{
 		var catclickcount = 0;
-        $('#catphoto').click(function() {
+        $('.catphoto').click(function() {
             catclickcount = catclickcount + 1;  
             $('#catcount').text(catclickcount);   
         });    
+
+        $("#catname1").text("Yoda cat");
+        
+        $("#catname2").text("Darth wader")
     });
